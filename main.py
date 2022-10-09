3e76ebceecd4150abea3d605612ef56abeb6ae33
zhiyangxiao <812381505@QQ.com@domain.com>
2022-10-09 20:29:40 +0800

    add README.md

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print(torch.__version__)
+    print("jajajajaj")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
